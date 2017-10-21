1174605c50371bd4bb21a3a39b5dc867ecbb5d85
sharon zheng <sharzheng@gmail.com>
2017-10-21 02:30:28 -0400

    add other directions

--- mingle.js
+++ mingle.js
@@ -12,16 +12,42 @@ function mingle() {
     const west = Array.from(document.getElementsByClassName('west'));
     const east = Array.from(document.getElementsByClassName('east'));
 
+    //calc n/s e/w separately TODO
     north.forEach(el => {
       el.style.top = app_size.top - window.getComputedStyle(el, null).getPropertyValue('font-size').split('px')[0] / 1.25;
 
-      const percentage = `${((window_width / 2 - el.getBoundingClientRect().width / 2) / window_width) * 100}`;
+      if (!el.classList.contains('east') || !el.classList.contains('west')) {
+        const percentage = `${((window_width / 2 - el.getBoundingClientRect().width / 2) / window_width) * 100}`;
+        el.style.left = `calc(${percentage}%)`;
+      }
+    });
+
+    //this needs refactoring wow
+
+    east.forEach(el => {
+      const percentage = `${((app_size.right - el.getBoundingClientRect().width) / window_width) * 100}`;
+      console.log(percentage)
       el.style.left = `calc(${percentage}%)`;
+    });
+
+    west.forEach(el => {
+      const percentage = `${((app_size.left - el.getBoundingClientRect().width) / window_width) * 100}`;
+      console.log(percentage)
+      el.style.left = `calc(${percentage}%)`;
+    });
+
+    south.forEach(el => {
+      el.style.bottom = window_height - app_size.bottom - window.getComputedStyle(el, null).getPropertyValue('font-size').split('px')[0] / 1.5;
+
+      if (!el.classList.contains('east') || !el.classList.contains('west')) {
+        const percentage = `${((window_width / 2 - el.getBoundingClientRect().width / 2) / window_width) * 100}`;
+        el.style.left = `calc(${percentage}%)`;
+      }
     });
   }
 
   calc_position(); //onload
-  window.addEventListener('resize', calc_position, true); //implement debouncing
 };
 
 window.onload = mingle;
+window.addEventListener('resize', mingle, true);
